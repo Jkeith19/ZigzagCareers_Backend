4af8e020ba2a487e438f726f4862955e4074e3af
wEggoy20xx <wEggoy20xx@gmail.com>
2024-10-11 17:32:43 +0800

    Update Program.cs add authentication and authorization on API using Oauth2 JWT Bearer.

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,12 @@
+using API.Helper.OAuth2;
 using Data.Dto.Mapper;
 using Data.Entity.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Services.Library;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,19 +22,85 @@ builder.Services.AddScoped<ILibraryService, LibraryService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Library API",
+        Version = "v1"
+    });
+
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            ClientCredentials = new OpenApiOAuthFlow
+            {
+                TokenUrl = new Uri("https://localhost:7115/connect/token"),
+                Scopes = new Dictionary<string, string>
+                {
+                    ["LibraryAPI.read"] = "Read access to Library API"
+                }
+            }
+        }
+    });
+
+    options.OperationFilter<AuthorizeOperationFilter>();
+});
+
+// Configure Authentication and Authorization
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.Authority = "https://localhost:7115";
+    options.Audience = "LibraryAPI";
+    options.RequireHttpsMetadata = false;
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "https://localhost:7115",
+        ValidAudience = "LibraryAPI",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("@1q2w3e4r!!"))
+    };
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger()
+       .UseSwaggerUI(options =>
+       {
+           options.SwaggerEndpoint("/swagger/v1/swagger.json", "Library API");
+           options.OAuthClientId("library.client");
+           options.OAuthAppName("Library API");
+       });
 }
 
-app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
